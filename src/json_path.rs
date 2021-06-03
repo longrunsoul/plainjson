db7f72da2b466eacafdc85f8d8b3f582971ce1d6
longrunsoul <longrunsoul@outlook.com>
2021-06-03 17:40:31 +0800

    Implement skeleton code of function JsonPath::parse() and JsonPathPart::read_part()

--- src/json_path.rs
+++ src/json_path.rs
@@ -1,4 +1,10 @@
+use std::io::Read;
+use anyhow::{
+    Result,
+    bail,
+};
 
+use crate::peekable_codepoints::*;
 
 pub enum ArrayIndexSelector {
     Single(usize),
@@ -14,6 +20,7 @@ pub enum FilterExpressionOperand {
     Array(Vec<String>),
     Regex(String),
     Expression(Box<FilterExpression>),
+    JsonPath(Box<JsonPath>),
 }
 
 pub enum FilterExpressionOperator {
@@ -47,6 +54,70 @@ pub struct JsonPathPart {
     pub filter: Option<FilterExpression>,
 }
 
+impl JsonPathPart {
+    fn read_root<R>(peekable_cp: &mut PeekableCodePoints<R>) -> Result<Option<Self>>
+        where R: Read {
+
+
+        todo!()
+    }
+    fn read_current<R>(peekable_cp: &mut PeekableCodePoints<R>) -> Result<Option<Self>>
+        where R: Read {
+
+
+        todo!()
+    }
+    fn read_square_notation<R>(peekable_cp: &mut PeekableCodePoints<R>) -> Result<Option<Self>>
+        where R: Read {
+
+
+        todo!()
+    }
+    fn read_dot_notation<R>(peekable_cp: &mut PeekableCodePoints<R>) -> Result<Option<Self>>
+        where R: Read {
+
+
+        todo!()
+    }
+    pub fn read_part<R>(peekable_cp: &mut PeekableCodePoints<R>) -> Result<Option<Self>>
+        where R: Read {
+        match peekable_cp.peek_char(0)? {
+            None => Ok(None),
+            Some(c) => {
+                match c {
+                    '$' => JsonPathPart::read_root(peekable_cp),
+                    '@' => JsonPathPart::read_current(peekable_cp),
+                    '[' => JsonPathPart::read_square_notation(peekable_cp),
+                    _ => JsonPathPart::read_dot_notation(peekable_cp),
+                }
+            }
+        }
+    }
+}
+
 pub struct JsonPath {
     pub parts: Vec<JsonPathPart>,
+}
+
+impl JsonPath {
+    fn new(parts: Vec<JsonPathPart>) -> Self {
+        JsonPath {
+            parts
+        }
+    }
+    pub fn parse(path_str: &str) -> Result<Self> {
+        let mut path_parts = Vec::new();
+        let mut peekable_cp = PeekableCodePoints::new(path_str.as_bytes());
+        loop {
+            let part = JsonPathPart::read_part(&mut peekable_cp)?;
+            if part.is_none() {
+                break;
+            }
+
+            path_parts.push(part.unwrap());
+        }
+
+        let json_path = JsonPath::new(path_parts);
+        Ok(json_path)
+    }
 }
