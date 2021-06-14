a9f8f7d1dcd9dcb80dd07177930b86460e74d37f
longrunsoul <longrunsoul@outlook.com>
2021-06-14 17:09:42 +0800

    Rearranged lib.rs and added a little documentation comment.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
+//! This library provides:
+//! - JsonTag: A low-level JSON tag parser which reads JSON tags from an instance which implements trait std::io::Read
+//! - JsonNode: A JSON parser which supports getting or setting value from/to selected JSON nodes by JSONPath
+//!
+//! Note: Filter of JSONPath is not implemented yet, so currently filter expression is not supported.
+
 mod peekable_codepoints;
-pub mod json_tag;
-pub mod json_node;
-pub mod json_path;
+mod json_tag;
+mod json_node;
+mod json_path;
 mod filter_expression;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub use crate::json_tag::JsonTag;
+pub use crate::json_node::JsonNode;
